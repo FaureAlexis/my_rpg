4ff956eedf4b93b4ffa8c9e3aebe11ec1b6dbcc1
Edouard PRADIE <edouard.pradie@epitech.eu>
2022-04-25 13:46:07 +0200

    [ADD] new gestion map generation + new map disagne

--- src/map/game_test.c
+++ src/map/game_test.c
@@ -9,16 +9,29 @@
 #include "rpg.h"
 #include <SFML/Graphics.h>
 
+void init_back_window(map_t *map, char ***tab, int i)
+{
+    map->back = malloc(sizeof(game_object_t));
+    map->back->texture = sfTexture_createFromFile(tab[i][0], NULL);
+    map->back->sprite = sfSprite_create();
+    sfSprite_setTexture(map->back->sprite, map->back->texture, sfFalse);
+}
+
 void init_map_window(map_t *map, char ***tab, int i)
 {
-    map->object = malloc(sizeof(game_object_t));
-    map->object->texture = sfTexture_createFromFile(tab[i][0], NULL);
-    map->object->sprite = sfSprite_create();
-    sfSprite_setTexture(map->object->sprite, map->object->texture, sfFalse);
+    map->map = malloc(sizeof(game_object_t));
+    map->map->texture = sfTexture_createFromFile(tab[i][0], NULL);
+    map->map->sprite = sfSprite_create();
+    map->map->position.x = (my_atoi(tab[i][1]) * -1);
+    map->map->position.y = (my_atoi(tab[i][2]) * -1);
+    sfSprite_setTexture(map->map->sprite, map->map->texture, sfFalse);
 }
 
 void parse_tab(map_t *map, char ***tab, int i)
 {
+     if (my_arraylen(tab[i]) == 1) {
+        init_back_window(map, tab, i);
+    }
     if (my_arraylen(tab[i]) == 3) {
         init_map_window(map, tab, i);
     }
