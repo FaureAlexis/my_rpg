9df5921f481d07bfdda6c79957d3e957588ec884
Alexis <alexis.faure@epitech.eu>
2022-04-25 12:36:24 +0200

    [UPDATE] - update window title

--- src/init/init_window.c
+++ src/init/init_window.c
@@ -13,7 +13,7 @@ sfRenderWindow *init_window(void)
     sfVideoMode mode = {1920, 1080, 32};
     sfImage *favicon = NULL;
 
-    window = sfRenderWindow_create(mode, "Hasbullah le roi",
+    window = sfRenderWindow_create(mode, "Hasbullah Quest",
         sfResize | sfClose, NULL);
     if (!window)
         return NULL;
