c16c83eb4ac31428010c3b85db13c580aa4a8e10
Alexis <alexis.faure@epitech.eu>
2022-04-27 11:25:44 +0200

    [FREE] - Free help texts

--- src/free/free_text.c
+++ src/free/free_text.c
@@ -12,6 +12,12 @@ static void destroy_text(sfText *text)
     sfText_destroy(text);
 }
 
+static void destroy_help_text(main_game_t *game)
+{
+    destroy_text(game->help->goal_text);
+    destroy_text(game->help->help_text);
+}
+
 static void destroy_settings_text(main_game_t *game)
 {
     destroy_text(game->settings->res_text);
