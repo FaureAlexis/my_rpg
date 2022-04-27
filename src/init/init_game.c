69ade67c01bf3d11f3406e25aedb02d80e874779
Alexis <alexis.faure@epitech.eu>
2022-04-27 10:31:12 +0200

    [ADD] - init_help function

--- src/init/init_game.c
+++ src/init/init_game.c
@@ -48,6 +48,23 @@ int init_settings(main_game_t *game)
     return EXIT_SUCCESS;
 }
 
+int init_help(main_game_t *game)
+{
+    sfFont *gravity = sfFont_createFromFile("./assets/font/fs-gravity.ttf");
+    game->help = malloc(sizeof(help_t));
+    if (!game->help || !gravity)
+        return EPITECH_ERROR;
+    game->help->goal_text = sfText_create();
+    game->help->help_text = sfText_create();
+    sfText_setFont(game->help->goal_text, gravity);
+    sfText_setFont(game->help->help_text, gravity);
+    sfText_setColor(game->help->goal_text, sfWhite);
+    sfText_setColor(game->help->help_text, sfWhite);
+    sfText_setCharacterSize(game->help->goal_text, 34);
+    sfText_setCharacterSize(game->help->help_text, 34);
+    return EXIT_SUCCESS;
+}
+
 int init_all(main_game_t *game)
 {
     if (create_menus(game) == EPITECH_ERROR)
