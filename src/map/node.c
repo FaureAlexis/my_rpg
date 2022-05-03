7b7b8b0b8299be1129c0341dc10ee5e0fec06215
Edouard PRADIE <edouard.pradie@epitech.eu>
2022-05-03 11:40:04 +0200

    [ADD] special obstacle + coding style

--- src/map/node.c
+++ src/map/node.c
@@ -22,6 +22,21 @@ obstacle_t *add_node_to_obstacle(obstacle_t *head, obstacle_t *node)
     return head;
 }
 
+speobstacle_t *add_node_to_speobstacle(speobstacle_t *head, speobstacle_t *node)
+{
+    speobstacle_t *tmp = head;
+
+    if (head == NULL) {
+        head = node;
+    } else {
+        while (tmp->next != NULL) {
+            tmp = tmp->next;
+        }
+        tmp->next = node;
+    }
+    return head;
+}
+
 mobe_t *add_node_to_mobe(mobe_t *head, mobe_t *node)
 {
     mobe_t *tmp = head;
