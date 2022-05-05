126bf0e53a725962e4ae838fa8fedf3764014633
Edouard PRADIE <edouard.pradie@epitech.eu>
2022-05-05 14:15:42 +0200

    [ADD] :rocket: --> finish map whith tree and bush

--- src/map/hasbulla.c
+++ src/map/hasbulla.c
@@ -16,19 +16,13 @@ void limit_hasbulla(mobe_t *mob, player_t *player)
 
 void display_hasbulla(mobe_t *mob, main_game_t *game)
 {
+    limit_hasbulla(mob, game->player);
     mob->attack_clock->time = sfClock_getElapsedTime(mob->my_clock->clock);
     mob->attack_clock->seconds = mob->my_clock->time.microseconds / 1000000.0;
     mob->my_clock->time = sfClock_getElapsedTime(mob->my_clock->clock);
     mob->my_clock->seconds = mob->my_clock->time.microseconds / 1000000.0;
     if (mob->attack == true && mob->attack_clock->seconds >= 0.08) {
-        mob->object->position.x += (game->player->object->position.x -
-        mob->object->position.x) / (sqrt(pow(game->player->object->position.x -
-        mob->object->position.x, 2) + pow(game->player->object->position.y -
-        mob->object->position.y, 2)));
-        mob->object->position.y += (game->player->object->position.y -
-        mob->object->position.y) / (sqrt(pow(game->player->object->position.x -
-        mob->object->position.x, 2) + pow(game->player->object->position.y -
-        mob->object->position.y, 2)));
+        mob_action_move(mob, game->player);
         sfRectangleShape_setPosition(mob->hitbox_shape,
         (sfVector2f){mob->object->position.x - 30,
         mob->object->position.y - 20});
@@ -41,5 +35,4 @@ void display_hasbulla(mobe_t *mob, main_game_t *game)
     sfSprite_setTextureRect(mob->object->sprite, mob->object->rect);
     sfSprite_setPosition(mob->object->sprite, mob->object->position);
     sfRenderWindow_drawSprite(game->w, mob->object->sprite, NULL);
-    limit_hasbulla(mob, game->player);
 }
