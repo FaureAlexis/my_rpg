5a253f91c552d1f0021434b4505270a37994b226
Gradrien <adrien.ricou@epitech.eu>
2022-05-06 15:49:00 +0200

    [COD.STY] :zap: --> Deleted comments inside function

--- src/scenes/instance/starting_scene/starting_save_scene.c
+++ src/scenes/instance/starting_scene/starting_save_scene.c
@@ -13,7 +13,5 @@ int starting_save_scene(main_game_t *game)
     game->menu_depth = 2;
     sfMusic_pause(game->mnu->gameplay->theme);
     sfMusic_play(game->btn->big->play_b->sound);
-    // sfView_reset(game->basic_view, (sfFloatRect){0, 0, 1920, 1080});
-    //sfRenderWindow_setView(game->w, game->basic_view);
     return EXIT_SUCCESS;
 }
